fc6e17797e9d435accb5f3f6777e0d48bb922e04
Programming Hero <programming.hero1@gmail.com>
2021-10-17 15:59:42 -0500

    my initial node express server

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.send('WOW. I am Excited to learn Node and express with nodemon. automatic restart');
+});
+
+const users = [
+    { id: 0, name: 'Shabana', email: 'Shabana@gmail.com', phone: '01788888888' },
+    { id: 1, name: 'Shabnoor', email: 'Shabnoor@gmail.com', phone: '01788888888' },
+    { id: 2, name: 'Shrabonti', email: 'Shrabonti@gmail.com', phone: '01788888888' },
+    { id: 3, name: 'Suchorita', email: 'Suchorita@gmail.com', phone: '01788888888' },
+    { id: 4, name: 'Soniya', email: 'Soniya@gmail.com', phone: '01788888888' },
+    { id: 5, name: 'Susmita', email: 'Susmita@gmail.com', phone: '01788888888' },
+]
+
+app.get('/users', (req, res) => {
+    const search = req.query.search;
+    // use query parameter
+    if (search) {
+        const searchResult = users.filter(user => user.name.toLocaleLowerCase().includes(search));
+        res.send(searchResult);
+    }
+    else {
+        res.send(users)
+    }
+});
+
+// app.METHOD
+app.post('/users', (req, res) => {
+    console.log('req body', req.body);
+    const newUser = req.body;
+    newUser.id = users.length;
+    users.push(newUser);
+    console.log('hitting the post', newUser)
+    res.json(newUser);
+})
+
+// dynamic api
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const user = users[id]
+    res.send(user);
+})
+
+app.get('/fruits', (req, res) => {
+    res.send(['mango', 'oranges', 'banana', 'apple'])
+})
+
+app.get('/fruits/mangoes/fazli', (req, res) => {
+    res.send('Yummy Yummy tok marka fazli');
+})
+
+app.listen(port, () => {
+    console.log('Listening to port ', port);
+})
